7b4d09855137edfa0994decbcfdf4e22c7fd6627
Ron Kurr <rkurr@transparent.com>
2015-02-23 12:45:40 -0500

    Removed all the outdated sample code

--- manuscript/packer/ansible/scripts/passwordless-sudo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo '%sudo    ALL=(ALL)  NOPASSWD:ALL' >> /etc/sudoers.d/passowrdless
-echo 'Defaults !requiretty' >> /etc/sudoers.d/passwordless
-chmod 0400 /etc/sudoers.d/passwordless
-
--- manuscript/packer/build-utopic-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PACKER_LOG=activate
-#export PACKER_LOG_PATH=/tmp
-
-packer validate utopic-x64-server.json
-packer build utopic-x64-server.json
